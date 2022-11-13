9bba7e44fc21cb9cee55e3fa6f2adb30efc231c2
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2022-11-13 22:44:59 +0530

    authenticate login

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,8 +2,18 @@ const bcrypt = require("bcrypt");
 const generateOutput = require("../helpers/generateOutput");
 const userRepository = require("../repositories/userRepository");
 const { generateAccessToken } = require("../helpers/accessToken");
+const { userLogInSchema } = require("../validationSchemas/userSchema");
 
 async function signInUser(data) {
+  if (!data) {
+    return generateOutput(400, "Invalid request");
+  }
+  try {
+    await userLogInSchema.validateAsync({ ...data });
+  } catch (error) {
+    return generateOutput(401, "Validation error!");
+  }
+
   try {
     const user = await userRepository.getUser(data.email);
     if (!user) {
